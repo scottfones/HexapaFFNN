442f8bb2cb4e709cc480b703f089821f08e380ff
Scott Fones <scott.fones@gmail.com>
2020-12-14 02:46:46 -0500

    works to textbook spec

---
+++ minimax.py
@@ -0,0 +1,38 @@
+import game
+
+
+def minimax_search(state):
+    player = state[0]
+    value, move = max_value(state)
+
+    return move
+
+
+def max_value(state):
+    if game.is_terminal(state):
+        return game.utility(state), None
+
+    v = -20
+    for act in game.actions(state):
+        v2, act2 = min_value(game.result(state, act))
+
+        if v2 > v:
+            v = v2
+            move = act
+
+    return v, move
+
+
+def min_value(state):
+    if game.is_terminal(state):
+        return game.utility(state), None
+
+    v = 20
+    for act in game.actions(state):
+        v2, act2 = max_value(game.result(state, act))
+
+        if v2 < v:
+            v = v2
+            move = act
+
+    return v, move
