b6f5fa8153b44029054e6e0d85d22ae051de5d20
Scott Fones <scott.fones@gmail.com>
2020-12-14 01:24:43 -0500

    initial commit.

---
+++ main.py
@@ -0,0 +1,14 @@
+"""main.py initializes the game"""
+
+import gameboard
+
+
+def main():
+    """Create the board and start the game."""
+
+    g = gameboard.GameBoard()
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
