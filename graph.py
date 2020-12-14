0f1e099a261bcaec1fa013ee7051e0afb3d146ec
Scott Fones <scott.fones@gmail.com>
2020-12-14 17:21:25 -0500

    creation and classify done

--- graph.py
+++ graph.py
@@ -1,28 +1,14 @@
 """graph.py satisfies Part 3 of the writeup.
 
-Sourced from textbook chapter 21 and https://towardsdatascience.com/neural-net-from-scratch-using-numpy-71a31f6e3675
+Sourced from textbook chapter 21
 
 Notes:
     From page 752
-        a_j = g_j(w'x)
+        a_j = g_j(w.Tx)
             a_j = output of unit j
             g_j = nonlinear activation function
-            w' = vector of weights leading into unit j, including bias unit 0 -> w_0,j
+            w.T = vector of weights leading into unit j, including bias unit 0 -> w_0,j
             x = vector of inputs to unit j, including bias unit 0 -> 1
-    
-    From site:
-        Dimensions:
-            n_x = number of inputs
-            n_h = nodes in hidden layer
-            n_y = nodes of output layer
-
-            a_1 = n_h, 1
-            w_1 = n_h, n_x
-            b_1 = n_h, 1
-
-            a_end = n_y, 1
-            w_end = n_y, n_h
-            b_end = n_y, 1
 
     We define:
         a_i: outputs of layer i
@@ -35,42 +21,65 @@ Notes:
 import numpy as np
 
 
-class FeedForwardNet:
-    def __init__(self, alpha, act_func, h_layers, in_matrix, num_h, num_in, num_out):
-        self.alpha = alpha
-        self.activ_func = act_func
-        self.hidden_layers = h_layers
-        self.input_matrix = in_matrix
-        self.num_inputs = num_in
-        self.num_hidden = num_h
-        self.num_output = num_out
+def activation_sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
-        self.network_layers = []
+def activation_relu(x):
+    return np.maximum(0,x)
+
+
+class FeedForwardNet:
+    def __init__(self, act_func, alpha, h_layers, h_units, in_units, out_units):
+        self.activ_func = act_func
+        self.alpha = alpha
+        self.hidden_layers = h_layers
+        self.hidden_units = h_units
+        self.in_units = in_units
+        self.out_units = out_units
+
         self.create_network()
 
     def create_network(self):
-        # Input Layer
-        self.network_layers.append((self.input_matrix, None))
+        self.network_layers = []
 
         # Hidden Layers
-        n_x = self.input_matrix.shape[0]
-        for _ in range(self.hidden_layers):
-            tmp_w = np.random.randn(self.num_hidden, n_x) * self.alpha
-            tmp_b = np.zeros((self.num_hidden, 1))
+        for i in range(self.hidden_layers):
+            if i > 0: 
+                if type(self.network_layers[i-1]) is tuple:
+                    m = self.network_layers[i-1][0].shape[1]
+                else:
+                    m = self.network_layers[i-1].shape[1]
+            else:
+                m = self.in_units
+            n = self.hidden_units
+            
+            tmp_w = np.random.randn(m, n) * self.alpha
+            tmp_b = np.zeros((n, 1))
             self.network_layers.append((tmp_w, tmp_b))
 
         # Output Layer
-        tmp_w = np.random.randn(self.num_hidden, n_x) * self.alpha
-        tmp_b = np.zeros((self.num_hidden, 1))
+        tmp_w = np.random.randn(self.hidden_units, self.out_units) * self.alpha
+        tmp_b = np.zeros((self.out_units, 1))
         self.network_layers.append((tmp_w, tmp_b))
 
-    def calc_forward(self):
+    def classify(self, train_data):
         self.forward = []
 
-        for i in range(1, len(self.network_layers)):
-            vec_b = self.network_layers[i][1]
-            vec_w = self.network_layers[i][0]
-            vec_x = self.network_layers[i - 1][0]
+        # Input to Hidden Transition
+        b_0 = self.network_layers[0][1]
+        w_0 = self.network_layers[0][0]
+        x = train_data
 
-            a_i = self.activ_func(np.dot(vec_w, vec_x) + vec_b)
-            self.network_layers.append(a_i)
+        a_i = self.activ_func(w_0.T @ x + b_0)
+        self.forward.append(a_i)
+
+        for i in range(1, len(self.network_layers)):
+            b_i = self.network_layers[i][1]
+            w_i = self.network_layers[i][0]
+            a_im1 = self.forward[i - 1]
+
+            a_i = self.activ_func(w_i.T @ a_im1 + b_i)
+            self.forward.append(a_i)
+
+    def update_weights(self, train_ans):
+        
