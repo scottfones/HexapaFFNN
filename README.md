28df0d7cd27f534cc46162b7e8881b76e7680806
Scott Fones <scott.fones@gmail.com>
2020-12-21 16:43:31 -0500

    reworked. create networks and pass to test funcs.

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # HexapaFFNN
 HexapaFFNN provides solutions to hexapawn using Feed Forward Neural Networks
 
-
 Written with python 3.8.6. Imports numpy and random
 
 Running main.py as a script `python main.py` should run through everything.
--- main.py
+++ main.py
@@ -5,23 +5,19 @@ import random
 
 
 def main():
-    """Create the board and start the game."""
-    ps = minimax.gen_states()
-    pt = minimax.gen_table(ps)
-    net = graph.FeedForwardNet(graph.activation_sigmoid, 0.01, 20, 13, 10, 9)
+    """Entry point for script.
 
-    for i in range(500):
-        d = random.choice(pt)
-        net.classify(d)
-        net.update_weights(pt[d])
+    1. Demonstate policy table
 
-    for i in range(5):
-        d = random.choice(pt)
-        net.classify(d)
+    2. Defines networks for adder and hexapawn, then test.
+    """
 
-        print(f"Trying: {d[0]}")
-        print(f"Classified: {net.forward_ai[-1]}")
-        print(f"Correct Classification: {d[1]}")
+    net = graph.FeedForwardNet(graph.activation_relu, 0.01, 1, 2, 2, 2)
+    graph.test_adder(net)
+
+    net = graph.FeedForwardNet(graph.activation_relu, 0.01, 20, 13, 10, 9)
+    graph.test_hexa(net)
+
 
 
 if __name__ == "__main__":
